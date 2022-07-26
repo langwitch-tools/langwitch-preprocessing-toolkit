233c1dab22b9d2dca4e2b84ab3d24df2f077072d
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-26 15:21:36 +1200

    Add files via upload

---
+++ clients/client_deps.ts
@@ -0,0 +1,3 @@
+export * as R from "https://esm.sh/ramda@0.28.0";
+export * as stream from "https://deno.land/std@0.148.0/streams/mod.ts";
+export { parse } from "https://deno.land/std@0.148.0/flags/mod.ts";
