530085649d5b3cf3a35d8055b9cce6ea66700622
Catherine Koshka <48640397+ckoshka@users.noreply.github.com>
2022-07-26 15:21:20 +1200

    Delete clients directory

--- clients/client_deps.ts
+++
@@ -1,3 +0,0 @@
-export * as R from "https://esm.sh/ramda@0.28.0";
-export * as stream from "https://deno.land/std@0.148.0/streams/mod.ts";
-export { parse } from "https://deno.land/std@0.148.0/flags/mod.ts";
